accf021acbaedc37f38d3621859f2d834f550a1d
Wako <dulo.rodrigo@gmail.com>
2021-04-24 04:18:17 -0500

    New glitches

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/4k1k0/gopher-glitch
+
+go 1.16
+
+require (
+	github.com/jpoz/glitch v0.0.0-20180920181737-06b2670973c7
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"math/rand"
 	"os"
-	"strconv"
+	"time"
 
 	"github.com/jpoz/glitch"
 )
@@ -24,25 +26,106 @@ func main() {
 	generate(*file_name, *iterations)
 }
 
-func check(e error) {
-	if e != nil {
-		panic(e)
+func generateRandomNumber(min, max int) int {
+	rand.Seed(time.Now().UnixNano())
+	diff := max - min
+	if diff == 0 {
+		return rand.Intn(max)
 	}
+	return rand.Intn(diff) + min
+}
+
+func generateWidthAndHeight(mw, mh int) (width, height int) {
+	width = generateRandomNumber(mw, mh)
+	height = generateRandomNumber(mw, mh)
+	return
+}
+
+func generateRGB() (r, g, b float64) {
+	rand.Seed(time.Now().UnixNano())
+	min := 0
+	max := 255
+	r = float64(rand.Intn(max-min) + min)
+	g = float64(rand.Intn(max-min) + min)
+	b = float64(rand.Intn(max-min) + min)
+	return
 }
 
 func generate(file string, iterations int) {
 	gl, err := glitch.NewGlitch("./" + file)
-	check(err)
-	gl.Copy()
+	if err != nil {
+		panic("could ont create new glitch")
+	}
+	width, height := gl.Bounds.Max.X, gl.Bounds.Max.Y
+
+	// Directory
+
+	directory := fmt.Sprintf("glich_%s", file)
+	if _, err := os.Stat(directory); os.IsNotExist(err) {
+		os.Mkdir(directory, os.ModePerm)
+	}
 
 	for i := 0; i < iterations; i++ {
-		n := strconv.Itoa(i + 1)
-		gl.VerticalTransposeInput(i+1*50, 100, true)
-		gl.ChannelShiftLeft()
+		rand.Seed(time.Now().UnixNano())
+		gl.Seed(rand.Int63())
 
-		newFile := fmt.Sprintf("./glitch_" + n + "_" + file)
-		f, err := os.Create(newFile)
-		check(err)
+		gl.Copy()
+
+		filename := fmt.Sprintf("./glitch_%d_%s", i+1, file)
+		fmt.Printf("[%d/%d] Generating %s\n", i+1, iterations, filename)
+
+		// Lineas verticales
+
+		//{
+		//	w, h := generateWidthAndHeight(width, height)
+		//	gl.VerticalTransposeInput(w/10, h/10, true)
+		//}
+
+		// Horizontales
+
+		//{
+		//	w, h := generateWidthAndHeight(width, height)
+		//	gl.TransposeInput(h/10, w/10, true)
+		//}
+
+		{
+			w, h := generateWidthAndHeight(width, height)
+			if w < h {
+				gl.ChannelShiftLeft()
+				gl.HalfLifeLeft(w, width)
+			} else {
+				gl.ChannelShiftRight()
+				gl.HalfLifeRight(h, width)
+			}
+		}
+
+		//{
+		//	w, h := generateWidthAndHeight(width, height)
+		//	if w < h {
+		//		gl.BlueBoost()
+		//	} else {
+		//		gl.GreenBoost()
+		//	}
+		//}
+
+		//{
+		//	r, g, b := generateRGB()
+		//	gl.Noise(r, g, b, 0.07)
+		//}
+
+		gl.GhostStreach()
+		gl.RedBoost()
+
+		f, err := os.Create(filename)
+		if err != nil {
+			panic("could not create new file")
+		}
 		gl.Write(f)
+
+		// move
+		err = os.Rename(filename, fmt.Sprintf("%s/%s", directory, filename))
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 }
